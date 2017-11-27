f5729df9453d2ef7f5a70a7ae452e4f44a9349d3
jason <liangdj@joosure.com>
2017-11-27 16:59:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jasonliang/tinkerhotrepair/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.jasonliang.tinkerhotrepair;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jason Liang on 2017/11/27.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/jasonliang/tinkerhotrepair/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.example.jasonliang.tinkerhotrepair;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends BaseActivity {
+
+    private TextView tv ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tv = findViewById(R.id.tv) ;
+        FloatingActionButton fab =  findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        // TODO 模拟错误
+        int i =1 ;
+        int j =0 ;
+
+        int sum = i/j ;
+        tv.setText("结果是：" + sum);
+    }
+}
